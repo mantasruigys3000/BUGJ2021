9f05ad85ca03ff2f5a8fc08254e60562d8967708
mantas <mantasruigys101@gmail.com>
2021-01-24 18:17:49 +0000

    old lobbying changed

--- bloons_tower_offence/Assets/scripts/CustomNetworkManager.cs
+++ bloons_tower_offence/Assets/scripts/CustomNetworkManager.cs
@@ -6,35 +6,7 @@ using Mirror;
 
 public class CustomNetworkManager : NetworkManager
 {
-    public class Match {
-        public System.Guid matchId;
-
-        public Match(System.Guid _id) {
-            this.matchId = _id;
-
-        }
-    }
-
-    public List<Match> matches;
-
-
-    public override void OnStartHost() {
-        matches = new List<Match>();
-        Match firstMatch = new Match(System.Guid.NewGuid());
-        matches.Add(firstMatch);
-
-
-
-        base.OnStartHost();
-    }
-
     public override void OnServerAddPlayer(NetworkConnection conn) {
-        Transform startPos = GetStartPosition();
-        GameObject player = startPos != null
-            ? Instantiate(playerPrefab, startPos.position, startPos.rotation)
-            : Instantiate(playerPrefab);
-        //player.GetComponent<NetworkMatchChecker>().matchId = matches[0].matchId;
-
-        NetworkServer.AddPlayerForConnection(conn, player);
+        base.OnServerAddPlayer(conn);
     }
 }
