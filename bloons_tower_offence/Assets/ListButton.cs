95069a4a78cdbfc66cc6b7f73bf90aa8e71d532b
mantas <mantasruigys101@gmail.com>
2021-01-25 18:19:24 +0000

    pain

---
+++ bloons_tower_offence/Assets/ListButton.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ListButton : MonoBehaviour
+{
+    public Text lobby;
+    public Text lobby_name;
+    public Text map;
+    public Text map_name;
+    public Text players;
+    public Text max_players;
+    public NetworkUiController ui_controller;
+
+
+}
