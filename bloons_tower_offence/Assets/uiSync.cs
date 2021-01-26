b99e3320b4a8c2b2ffe7f5d463221d4841a4ce3d
Teodor Sava <mice.graff@gmail.com>
2021-01-26 22:30:43 +0000

    last push

---
+++ bloons_tower_offence/Assets/uiSync.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class uiSync : MonoBehaviour
+{
+    public GameObject health;
+    public GameObject rocketAmmo;
+    public GameObject nailsAmmo;
+    public GameObject rayAmmo;
+    public GameObject chosenWpn;
+    public List<GameObject> weapons;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
